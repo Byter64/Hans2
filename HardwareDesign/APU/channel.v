95c8e25d4fa29512083e585676d9af147a7ce5f0
Byter64 <byter64@gmx.de>
2024-10-18 20:09:48 +0200

    Behoben: Channel dreht welle jetzt durch bitnegation Hinzugüfgt: Test topmodul für channel

--- HardwareDesign/APU/channel.v
+++ HardwareDesign/APU/channel.v
@@ -1,17 +1,20 @@
 module channel 
+#(
+    parameter WIDTH = 16
+)
 (
     input clk,
     input rstn,
 
-    input[15:0] volume, //Signed int. Note, that only the highest 4 bit will be used for actual sound volume
-    input[15:0] duty_cycle, //In ticks. How many ticks within the period is the wave high
-    input[15:0] period, //Period time in ticks
+    input[WIDTH-1:0] volume, //Signed int. Note, that only the highest 4 bit will be used for actual sound volume
+    input[WIDTH-1:0] duty_cycle, //In ticks. How many ticks within the period is the wave high
+    input[WIDTH-1:0] period, //Period time in ticks
 
-    output[15:0] wave
+    output[WIDTH-1:0] wave
 );
 
-reg[15:0] period_counter = 0;
-assign wave = period_counter < duty_cycle ? volume : -volume;
+reg[WIDTH-1:0] period_counter = 0;
+assign wave = period_counter < duty_cycle ? volume : ~volume;
 
 always @(posedge clk) begin
     if(rstn == 0) begin
