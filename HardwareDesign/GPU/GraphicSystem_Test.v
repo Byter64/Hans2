ba66ff4a3440d02e95a557a87199264f62e58100
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-01-10 16:12:45 +0100

    Added mem_valid signal to GraphicSystem.v Added regs and wires to GraphicSystem_Test.v

--- HardwareDesign/GPU/GraphicSystem_Test.v
+++ HardwareDesign/GPU/GraphicSystem_Test.v
@@ -7,22 +7,22 @@ module GraphicSystem_Test (
 );
 
 
-wire swapBuffers;
-wire isVSynced;
-wire gpu_MemData;
-wire gpu_MemAddr;
+reg  swapBuffers;
+reg  isVSynced;
+reg[15:0]  gpu_MemData;
+wire[31:0] gpu_MemAddr;
 wire gpu_MemRead;
-wire gpu_CtrlAddress;
-wire gpu_CtrlAddressX;
-wire gpu_CtrlAddressY;
-wire gpu_CtrlImageWidth;
-wire gpu_CtrlWidth;
-wire gpu_CtrlHeight;
-wire gpu_CtrlX;
-wire gpu_CtrlY;
-wire gpu_CtrlDraw;
-wire gpu_CtrlClearColor;
-wire gpu_CtrlClear;
+reg[31:0]  gpu_CtrlAddress;
+reg[15:0]  gpu_CtrlAddressX;
+reg[15:0]  gpu_CtrlAddressY;
+reg[15:0]  gpu_CtrlImageWidth;
+reg[10:0]  gpu_CtrlWidth;
+reg[9:0]  gpu_CtrlHeight;
+reg[10:0]  gpu_CtrlX;
+reg[9:0]  gpu_CtrlY;
+reg  gpu_CtrlDraw;
+reg[15:0]  gpu_CtrlClearColor;
+reg  gpu_CtrlClear;
 wire gpu_CtrlBusy;
 
 GraphicSystem graphicSystem 
@@ -31,23 +31,24 @@ GraphicSystem graphicSystem
     .cpuClk(clk_25mhz),
     .reset(1'b0),
     .gpdiDp(gpdi_dp),
-    .swapBuffers,
-    .isVSynced,
-    .gpu_MemData,
-    .gpu_MemAddr,
-    .gpu_MemRead,
-    .gpu_CtrlAddress,
-    .gpu_CtrlAddressX,
-    .gpu_CtrlAddressY,
-    .gpu_CtrlImageWidth,
-    .gpu_CtrlWidth,
-    .gpu_CtrlHeight,
-    .gpu_CtrlX,
-    .gpu_CtrlY,
-    .gpu_CtrlDraw,
-    .gpu_CtrlClearColor,
-    .gpu_CtrlClear,
-    .gpu_CtrlBusy
+    .swapBuffers(swapBuffers),
+    .isVSynced(isVSynced),
+    .gpu_MemData(gpu_MemData),
+    .gpu_MemValid(gpu_MemValid),
+    .gpu_MemAddr(gpu_MemAddr),
+    .gpu_MemRead(gpu_MemRead),
+    .gpu_CtrlAddress(gpu_CtrlAddress),
+    .gpu_CtrlAddressX(gpu_CtrlAddressX),
+    .gpu_CtrlAddressY(gpu_CtrlAddressY),
+    .gpu_CtrlImageWidth(gpu_CtrlImageWidth),
+    .gpu_CtrlWidth(gpu_CtrlWidth),
+    .gpu_CtrlHeight(gpu_CtrlHeight),
+    .gpu_CtrlX(gpu_CtrlX),
+    .gpu_CtrlY(gpu_CtrlY),
+    .gpu_CtrlDraw(gpu_CtrlDraw),
+    .gpu_CtrlClearColor(gpu_CtrlClearColor),
+    .gpu_CtrlClear(gpu_CtrlClear),
+    .gpu_CtrlBusy(gpu_CtrlBusy)
 );
 
 endmodule
