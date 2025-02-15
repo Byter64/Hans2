ad564e775513d5ed8019bebf032e2b7300bc85ef
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-02-15 17:08:33 +0100

    Addendum

---
+++ Software/Audio/AudioEncoder/AudioEncoder.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <fstream>
+#include <queue>
+
+//Delta has 12 Bits, with bitnumbers 1-12
+static int DELTA_MAX = (1 << 12) - 2; 
+static int DELTA_MIN = -(1 << 12);
+
+int16_t EncodeDelta(std::queue<bool>& bits, int32_t delta)
+{
+	if (delta > DELTA_MAX)
+		delta = DELTA_MAX;
+	else if (delta < DELTA_MIN)
+		delta = DELTA_MIN;
+
+	uint32_t u_delta = *(uint32_t*)&delta;
+	for(int i = 12; i >= 1; i--)
+	{
+		bits.push((u_delta >> i) & 1);
+	}
+
+	int16_t realDelta = delta;
+	realDelta &= 0xFFFE; //Remove low bit
+
+	return realDelta;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		printf("argument count != 2. EXITING\n");
+		printf("AudioDecoder \"source/file.raw\" \"destination/file.raw\"");
+		return -1;
+	}
+	
+	std::ifstream source = std::ifstream(argv[1], std::ios::binary);
+	std::ofstream destination = std::ofstream(argv[2], std::ios::binary);
+
+	std::queue<bool> bits;
+	int16_t lastSample = 0;
+
+	while (!source.eof())
+	{
+		unsigned char byte0 = static_cast<unsigned char>(source.get());
+		unsigned char byte1 = static_cast<unsigned char>(source.get());
+		uint16_t u_sample = byte0 + (byte1 << 8);
+		int16_t sample = *(int16_t*)&u_sample;
+		int32_t delta = sample - lastSample;
+
+		int16_t realDelta = EncodeDelta(bits, delta);
+
+
+		while (bits.size() >= 8)
+		{
+			unsigned char byte = 0;
+			for (int i = 7; i >= 0; i--)
+			{
+				byte += bits.front() << i;
+				bits.pop();
+			}
+			destination.put((byte));
+		}
+
+		lastSample = lastSample + realDelta; //Using the actual delta here has the positive of not accumulating errors
+	}
+
+
+	source.close();
+	destination.close();
+	return 0;
+}
