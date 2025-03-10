0619d2cb00dba2cc1039eb3d400c6109287c558a
Jonathan Hager <mail@hager-jonathan.de>
2025-03-10 20:59:10 +0100

    Change the hardware build system This presents a simpler interface to the user

--- cmake/hardware_targets.cmake
+++ cmake/hardware_targets.cmake
@@ -1,60 +1,59 @@
 # Target definition for subdirectories
-function(add_simulation testbench, sources)
-  add_custom_command(OUTPUT ${testbench}.out
+function(add_simulation
+    name, testbench, sources)
+  add_custom_command(OUTPUT ${name}.out
     DEPENDS ${sources}
-    COMMAND iverilog -g2012 -s ${testbench} -o ${CMAKE_CURRENT_BINARY_DIR}/${testbench}.out ${sources}
+    COMMAND iverilog -g2012 -s ${testbench} -o ${CMAKE_CURRENT_BINARY_DIR}/${name}.out ${sources}
   )
 
-  add_custom_target(${testbench}_simulation
-    DEPENDS ${testbench}.out
-    COMMAND vvp ${CMAKE_CURRENT_BINARY_DIR}/${testbench}.out -lxt2
+  add_custom_target(${name}_simulation
+    DEPENDS ${name}.out
+    COMMAND vvp ${CMAKE_CURRENT_BINARY_DIR}/${name}.out -lxt2
   )
 endfunction()
 
-function(add_synthesis top, sources)
-  add_custom_command(OUTPUT ${top}.json
+function(add_synthesis
+    name, top, sources)
+  # Synthesis
+  add_custom_command(OUTPUT ${name}.json
     DEPENDS ${sources}
-    COMMAND yosys -m slang -f slang -p \"synth_ecp5 -top ${top}\; write_json ${CMAKE_CURRENT_BINARY_DIR}/${top}.json\" ${sources}
+    # COMMAND yosys -m slang -f "slang --ignore-unknown-modules" -p \"synth_ecp5 -top ${top}\; write_json ${CMAKE_CURRENT_BINARY_DIR}/${name}.json\" ${sources}
+    COMMAND yosys -p \"synth_ecp5 -top ${top}\; write_json ${CMAKE_CURRENT_BINARY_DIR}/${name}.json\" ${sources}
   )
 
-  add_custom_target(${top}_synthesis
-    DEPENDS ${top}.json
+  add_custom_target(${name}_synthesis
+    DEPENDS ${name}.json
   )
-endfunction()
 
-function(add_pr top)
-  add_custom_command(OUTPUT ${top}.config
-    DEPENDS ${top}.json
+  # Place and route
+  add_custom_command(OUTPUT ${name}.config
+    DEPENDS ${name}.json
     DEPENDS ulx3s.lpf
 
-    COMMAND nextpnr-ecp5 --85k --package CABGA381 --lpf ${CMAKE_CURRENT_SOURCE_DIR}/ulx3s.lpf --lpf-allow-unconstrained --json ${CMAKE_CURRENT_BINARY_DIR}/${top}.json --textcfg ${CMAKE_CURRENT_BINARY_DIR}/${top}.config
+    COMMAND nextpnr-ecp5 --85k --package CABGA381 --lpf ${CMAKE_CURRENT_SOURCE_DIR}/ulx3s.lpf --lpf-allow-unconstrained --json ${CMAKE_CURRENT_BINARY_DIR}/${name}.json --textcfg ${CMAKE_CURRENT_BINARY_DIR}/${name}.config
   )
 
-  add_custom_target(${top}_pr
-    DEPENDS ${top}.config
-  )
-endfunction()
-
-function(add_bitstream top)
-  add_custom_command(OUTPUT ${top}.bit
-    DEPENDS ${top}.config
-
-    COMMAND ecppack ${CMAKE_CURRENT_BINARY_DIR}/${top}.config ${CMAKE_CURRENT_BINARY_DIR}/${top}.bit
+  add_custom_target(${name}_pr
+    DEPENDS ${name}.config
   )
 
-  add_custom_target(${top}_bitstream
-    DEPENDS ${top}.bit
-  )
-endfunction()
+  # Bitstream
+  add_custom_command(OUTPUT ${name}.bit
+    DEPENDS ${name}.config
 
-function(add_upload top)
-  add_custom_target(${top}_upload
-    COMMAND fujprog ${CMAKE_CURRENT_BINARY_DIR}/${top}.bit
+    COMMAND ecppack ${CMAKE_CURRENT_BINARY_DIR}/${name}.config ${CMAKE_CURRENT_BINARY_DIR}/${name}.bit
   )
-endfunction()
 
-function(add_flash top)
-  add_custom_target(${top}_flash
-    COMMAND fujprog -j flash ${CMAKE_CURRENT_BINARY_DIR}/${top}.bit
+  add_custom_target(${name}_bitstream
+    DEPENDS ${name}.bit
+  )
+
+  # Upload targets
+  add_custom_target(${name}_upload
+    COMMAND fujprog ${CMAKE_CURRENT_BINARY_DIR}/${name}.bit
+  )
+
+  add_custom_target(${name}_flash
+    COMMAND fujprog -j flash ${CMAKE_CURRENT_BINARY_DIR}/${name}.bit
   )
 endfunction()
