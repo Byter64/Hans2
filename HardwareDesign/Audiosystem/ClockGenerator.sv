d3ff7ef37d9755b51f66b0c10b658159ba059fb7
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-03-28 09:46:44 +0100

    Removed delays from synthesis

--- HardwareDesign/Audiosystem/ClockGenerator.sv
+++ HardwareDesign/Audiosystem/ClockGenerator.sv
@@ -29,6 +29,9 @@ always_ff @(posedge clk_100mhz) begin
     clk_1024khz_counter <= clk_1024khz_counter + 1;
     if(clk_1024khz_counter + 1 == 49) begin
         clk_1024khz_counter <= 0;
+`ifdef SYNTHESIS
+        clk_1024khz <= ~clk_1024khz;
+`else
         clk_1024khz <= #1 ~clk_1024khz;
     end
 end
@@ -38,6 +41,9 @@ always_ff @(posedge clk_1024khz) begin
     clk_64khz_counter <= clk_64khz_counter + 1;
     if(clk_64khz_counter + 1 == 8) begin
         clk_64khz_counter <= 0;
+`ifdef SYNTHESIS
+        clk_64khz <= ~clk_64khz;
+`else
         clk_64khz <= #1 ~clk_64khz;
     end
 end 
@@ -47,6 +53,9 @@ always_ff @(posedge clk_1024khz) begin
     clk_32khz_counter <= clk_32khz_counter + 1;
     if(clk_32khz_counter + 1 == 16) begin
         clk_32khz_counter <= 0;
+`ifdef SYNTHESIS
+        clk_32khz <= ~clk_32khz;
+`else
         clk_32khz <= #1 ~clk_32khz;
     end
 end 
