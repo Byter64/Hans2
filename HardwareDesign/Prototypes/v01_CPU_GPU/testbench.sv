a0be18d77b24c6bf7cfccdb00665ff24687910ad
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-04-30 16:21:45 +0200

    Moved memory initialisation into the topmodule

--- HardwareDesign/Prototypes/v01_CPU_GPU/testbench.sv
+++ HardwareDesign/Prototypes/v01_CPU_GPU/testbench.sv
@@ -13,8 +13,6 @@ CPU_with_GPU Top
 	.gpdi_dp(gpdi_dp)
 );
 
-initial $readmemh("C:/Users/Yanni/Desktop/Hans2/HardwareDesign/Prototypes/v01_CPU_GPU/Software/firmware32.hex", Top.Memory.memory);
-
 initial begin
 	$dumpvars(0, testbench);
 	for(integer i = 0; i < 1000; i++) $dumpvars(0, Top.Memory.memory[i]);
