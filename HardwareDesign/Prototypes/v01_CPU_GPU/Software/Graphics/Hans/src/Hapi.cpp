5629eb8a36f5883d6f59d570940a13fd1d20f405
Byter64 <byter64@gmx.de>
2025-05-04 22:35:38 +0200

    Addendum

--- HardwareDesign/Prototypes/v01_CPU_GPU/Software/Graphics/Hans/src/Hapi.cpp
+++ HardwareDesign/Prototypes/v01_CPU_GPU/Software/Graphics/Hans/src/Hapi.cpp
@@ -12,23 +12,23 @@ struct ImageData
 
 volatile char* GPU_BLOCK 			= (char*)65536;
 volatile int* GPU_IMAGE_START		= (int*)(GPU_BLOCK + 0);
-volatile int* GPU_IMAGE_X			= (int*)(GPU_BLOCK + 1);
-volatile int* GPU_IMAGE_Y			= (int*)(GPU_BLOCK + 2);
-volatile int* GPU_IMAGE_WIDTH		= (int*)(GPU_BLOCK + 3);
-volatile int* GPU_EXCERPT_WIDTH		= (int*)(GPU_BLOCK + 4);
-volatile int* GPU_EXCERPT_HEIGHT	= (int*)(GPU_BLOCK + 5);
-volatile int* GPU_SCREEN_X			= (int*)(GPU_BLOCK + 6);
-volatile int* GPU_SCREEN_Y			= (int*)(GPU_BLOCK + 7);
-volatile int* GPU_CLEAR_COLOR		= (int*)(GPU_BLOCK + 8);
-volatile int* GPU_COMMAND_DRAW		= (int*)(GPU_BLOCK + 9);
-volatile int* GPU_COMMAND_CLEAR		= (int*)(GPU_BLOCK + 10);
-volatile int* GPU_IS_BUSY			= (int*)(GPU_BLOCK + 11);
+volatile int* GPU_IMAGE_X			= (int*)(GPU_BLOCK + 4);
+volatile int* GPU_IMAGE_Y			= (int*)(GPU_BLOCK + 8);
+volatile int* GPU_IMAGE_WIDTH		= (int*)(GPU_BLOCK + 12);
+volatile int* GPU_EXCERPT_WIDTH		= (int*)(GPU_BLOCK + 16);
+volatile int* GPU_EXCERPT_HEIGHT	= (int*)(GPU_BLOCK + 20);
+volatile int* GPU_SCREEN_X			= (int*)(GPU_BLOCK + 24);
+volatile int* GPU_SCREEN_Y			= (int*)(GPU_BLOCK + 28);
+volatile int* GPU_CLEAR_COLOR		= (int*)(GPU_BLOCK + 32);
+volatile int* GPU_COMMAND_DRAW		= (int*)(GPU_BLOCK + 36);
+volatile int* GPU_COMMAND_CLEAR		= (int*)(GPU_BLOCK + 40);
+volatile int* GPU_IS_BUSY			= (int*)(GPU_BLOCK + 44);
 
-volatile int* GPU_VSYNC				= (int*)(GPU_BLOCK + 12);
-volatile int* GPU_HSYNC				= (int*)(GPU_BLOCK + 13);
+volatile int* GPU_VSYNC				= (int*)(GPU_BLOCK + 48);
+volatile int* GPU_HSYNC				= (int*)(GPU_BLOCK + 52);
 
-volatile int* GPU_COMMAND_SWAP_BUFFERS= (int*)(GPU_BLOCK + 14);
-volatile int* VSYNC_BUFFER_SWAP		= (int*)(GPU_BLOCK + 15);
+volatile int* GPU_COMMAND_SWAP_BUFFERS= (int*)(GPU_BLOCK + 56);
+volatile int* VSYNC_BUFFER_SWAP		= (int*)(GPU_BLOCK + 60);
 
 int Hapi::Init()
 {
