63116edc97eb6bdd06d260de9a0bc4bd0b75c9d5
Byter64 <byter64@gmx.de>
2025-05-07 18:57:15 +0200

    Added more signals

--- HardwareDesign/SDRAM-Controller/AXILite_SDRAM.sv
+++ HardwareDesign/SDRAM-Controller/AXILite_SDRAM.sv
@@ -1,7 +1,8 @@
 module AXILite_SDRAM (
-    input logic clk_133mhz,
-
     //SDRAM
+
+    input logic clk_133mhz,
+    input logic resetn,
     output logic        sdram_clk,
     output logic        sdram_cke,
     output logic        sdram_csn,
@@ -37,20 +38,26 @@ module AXILite_SDRAM (
     input logic                              s_axil_rready,
 );
 
+logic[23:0] address;
+logic[15:0] write_data;
+logic       write_enable;
+logic[15:0] read_data;
+logic       read_ready;
+logic       is_busy;
 
 sdram_controller AXILiteSDRAM 
 (
     //Host Interface
-    .wr_addr(),
-    .wr_data(),
-    .wr_enable(),
-    .rd_addr(),
-    .rd_data(),
-    .rd_ready(),
-    .rd_enable(),
-    .busy(),
-    .rst_n(),
-    .clk(), //Expects 133 MHz
+    .wr_addr(address),
+    .wr_data(write_data),
+    .wr_enable(write_enable),
+    .rd_addr(address),
+    .rd_data(read_data),
+    .rd_ready(read_ready),
+    .rd_enable(read_enable),
+    .busy(is_busy),
+    .rst_n(resetn),
+    .clk(clk_133mhz), //Expects 133 MHz
     // SDRAM Interface
     .addr(sdram_a),
     .bank_addr(sdram_ba),
@@ -63,5 +70,103 @@ sdram_controller AXILiteSDRAM
     .data_mask_low(sdram_dqm[0]),
     .data_mask_high(sdram_dqm[1])
 );
+
+
+typedef enum logic[7:0] 
+{ 
+    IDLE,
+    READ_2B,
+    WRITE_2B,
+    WAIT_ACKNOWLEDGE
+} State;
+
+State state = IDLE;
+State next_state;
+
+always_comb begin
+    case (state)
+        : 
+        default: next_state = IDLE; 
+    endcase
+
+    if(!resetn)
+        next_state = IDLE;
+end
+
+//Address Write
+logic[ADDR_WIDTH-1:0] aw_address = 'b0;
+logic[31:0] aw_address_real;
+assign aw_address_real = s_axil_awvalid && s_axil_awready ? s_axil_awaddr : aw_address;
+always @(posedge aclk) begin
+		s_axil_awready <= 1;
+end
+
+always @(posedge aclk) begin
+	if (s_axil_awvalid && s_axil_awready) begin //Never add any other conditions. This is likely to break axi
+		aw_address <= s_axil_awaddr;
+    end
+end
+
+//Write
+always @(posedge aclk) begin
+		s_axil_wready <= 1;
+end
+
+
+always @(posedge aclk) begin
+	if (s_axil_wvalid && s_axil_wready) begin //Never add any other conditions. This is likely to break axi
+    if(s_axil_wstrb[0]) memory[aw_address_real[31:2]][7 -: 8] <= s_axil_wdata[7 -: 8];
+    if(s_axil_wstrb[1]) memory[aw_address_real[31:2]][15 -: 8] <= s_axil_wdata[15 -: 8];
+    if(s_axil_wstrb[2]) memory[aw_address_real[31:2]][23 -: 8] <= s_axil_wdata[23 -: 8];
+    if(s_axil_wstrb[3]) memory[aw_address_real[31:2]][31 -: 8] <= s_axil_wdata[31 -: 8];
+  end
+end
+
+//Write response
+always @(posedge aclk) begin
+	if (!aresetn)
+		s_axil_bvalid <= 0;
+	else if (!s_axil_bvalid || s_axil_bready) begin
+		s_axil_bvalid <= 1;
+  end
+end
+
+//Address Read
+logic[ADDR_WIDTH-1:0] ar_address = 'b0;
+logic[31:0] ar_address_real;
+assign ar_address_real = s_axil_arvalid && s_axil_arready ? s_axil_araddr : ar_address;
+always @(posedge aclk) begin
+		s_axil_arready <= 1;
+end
+
+always @(posedge aclk) begin
+	if (s_axil_arvalid && s_axil_arready) begin //Never add any other conditions. This is likely to break axi
+		ar_address <= s_axil_araddr;
+    end
+end
+
+//Read
+//This is not AXI compliant, but I could not think of a better way to invalidate s_axil_rdata if address is written at the sime time as data is read
+assign s_axil_rvalid = !aresetn ? 0 : !(s_axil_arvalid && s_axil_arready);
+
+logic[31:0] read_data;
+always @(*) begin
+    case (ar_address_real[1:0])
+      2'b00: s_axil_rdata = (read_data >>  0) & 'hFFFFFFFF;
+      2'b01: s_axil_rdata = (read_data >>  8) & 'hFF;
+      2'b10: s_axil_rdata = (read_data >> 16) & 'hFFFF;
+      2'b11: s_axil_rdata = (read_data >> 24) & 'hFF;
+    endcase
+end
+
+
+always @(posedge aclk) begin
+	if (!aresetn)
+		read_data <= 0;
+	else if (!s_axil_rvalid || s_axil_rready)
+	begin
+    read_data <= memory[ar_address_real[31:2]];
+	end
+end
     
 endmodule
