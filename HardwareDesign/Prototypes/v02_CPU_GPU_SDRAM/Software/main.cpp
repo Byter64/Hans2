3a397f99d3b3f7f08525f2a024006df6f4e335f6
Byter64 <byter64@gmx.de>
2025-05-10 20:29:42 +0200

    More deletions

--- HardwareDesign/Prototypes/v02_CPU_GPU_SDRAM/Software/main.cpp
+++ HardwareDesign/Prototypes/v02_CPU_GPU_SDRAM/Software/main.cpp
@@ -7,41 +7,7 @@
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
-
-volatile char* MEMORY_ADDRESS 		= (char*)24576;
-
-volatile int* GET_BUTTON_F1			= (int*)(MEMORY_ADDRESS + 512);
-volatile int* GET_BUTTON_F2			= (int*)(MEMORY_ADDRESS + 512 + 4);
-volatile int* GET_BUTTON_UP			= (int*)(MEMORY_ADDRESS + 512 + 8);
-volatile int* GET_BUTTON_DOWN		= (int*)(MEMORY_ADDRESS + 512 + 12);
-volatile int* GET_BUTTON_LEFT		= (int*)(MEMORY_ADDRESS + 512 + 16);
-volatile int* GET_BUTTON_RIGHT		= (int*)(MEMORY_ADDRESS + 512 + 20);
-
-volatile int* GET_NES_B				= (int*)(MEMORY_ADDRESS + 1024);
-volatile int* GET_NES_Y				= (int*)(MEMORY_ADDRESS + 1024 + 4);	
-volatile int* GET_NES_SELECT		= (int*)(MEMORY_ADDRESS + 1024 + 8);
-volatile int* GET_NES_START			= (int*)(MEMORY_ADDRESS + 1024 + 12);
-volatile int* GET_NES_UP			= (int*)(MEMORY_ADDRESS + 1024 + 16);
-volatile int* GET_NES_DOWN			= (int*)(MEMORY_ADDRESS + 1024 + 20);
-volatile int* GET_NES_LEFT			= (int*)(MEMORY_ADDRESS + 1024 + 24);
-volatile int* GET_NES_RIGHT			= (int*)(MEMORY_ADDRESS + 1024 + 28);
-volatile int* GET_NES_A				= (int*)(MEMORY_ADDRESS + 1024 + 32);
-volatile int* GET_NES_X				= (int*)(MEMORY_ADDRESS + 1024 + 36);
-volatile int* GET_NES_L				= (int*)(MEMORY_ADDRESS + 1024 + 40);
-volatile int* GET_NES_R				= (int*)(MEMORY_ADDRESS + 1024 + 44);
-
-
-struct Basket {
-    int x, y, vx;
-    bool active;
-};
-
-Basket baskets[2];
-
 void updateAnimation(int& ticks, int& frame_x, int frame_width, int max_frame);
-void moveBoat(int& x, int& y, int speed, bool up, bool down, bool left, bool right);
-void fireBasket(int x, int y, int direction);
-void updateBaskets();
 
 int main() {
     int water_ticks = 20, water_frame_x = 0;
@@ -82,34 +48,5 @@ void updateAnimation(int& ticks, int& frame_x, int frame_width, int max_frame) {
     if (--ticks == 0) {
         frame_x = (frame_x + frame_width) % max_frame;
         ticks = 20;
-    }
-}
-
-void moveBoat(int& x, int& y, int speed, bool up, bool down, bool left, bool right) {
-    if (up) y -= speed;
-    if (down) y += speed;
-    if (left) x -= speed;
-    if (right) x += speed;
-    if (y < 0) y = 0;
-    if (y > 200) y = 200;
-    if (x < 0) x = 0;
-    if (x > 352) x = 352;
-}
-
-void fireBasket(int x, int y, int direction) {
-    for (auto& b : baskets) {
-        if (!b.active) {
-            b = { x, y, direction, true };
-            break;
-        }
-    }
-}
-
-void updateBaskets() {
-    for (auto& b : baskets) {
-        if (b.active) {
-            b.x += b.vx;
-            if (b.x < 0 || b.x > 400) b.active = false;
-        }
     }
 }
