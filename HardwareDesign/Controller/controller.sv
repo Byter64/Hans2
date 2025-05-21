23fd0bae12adcc3d65cca15bcef8ca67f6a7883e
Timo <timey2002@gmail.com>
2025-05-21 10:10:56 +0200

    added axilite

--- HardwareDesign/Controller/controller.sv
+++ HardwareDesign/Controller/controller.sv
@@ -1,19 +1,47 @@
 module Controller (
     input  logic clk,
-    output logic[11:0] controller_btns,
+
+    input logic [31:0]                       s_axil_rdata,
+    input logic [1:0]                        s_axil_rresp,
+    input logic                              s_axil_rvalid,
+    output logic                             s_axil_rready,
 
     input  logic cont_data,
     output logic cont_clk = 0,
     output logic cont_activate
 );
 
-
+logic[11:0] controller_btns;
 
 logic[15:0] cont_state = 0;
 logic[10:0] state = START;
 localparam START = 0;
 localparam DATA_START = 3;
 localparam DATA_END = 3 + 2 * 16; //16 states, each 2 fast clock cycles
+
+
+/////////////////// R /////////////////////
+logic next_rvalid; //Assign your valid logic to this signal
+logic[31:0] next_rData; //Assign the data here
+assign next_rvalid = 1;
+assign next_rData = controller_btns;
+
+always_ff @(posedge aclk) begin
+    if (!aresetn)
+        s_axil_rvalid <= 0;
+    else if (!s_axil_rvalid || s_axil_rready) begin
+        s_axil_rvalid <= next_rvalid;
+    end
+end
+
+always_ff @(posedge aclk) begin
+    if (!aresetn)
+        s_axil_rdata <= 0;
+    else begin
+        s_axil_rdata <= next_rData;
+    end
+end
+///////////// AXI LITE END ////////////////////
 
 always_ff @(posedge clk) begin
     state <= state + 1;
