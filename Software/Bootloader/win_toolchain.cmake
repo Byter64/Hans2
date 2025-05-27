b058abf6023b8331ffbe6c2d2762d44befafe380
Byter64 <byter64@gmx.de>
2025-05-27 07:54:41 +0200

    Added proper cross compilation configuration with cmake

--- Software/Bootloader/win_toolchain.cmake
+++ Software/Bootloader/win_toolchain.cmake
@@ -1,26 +1,24 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR riscv32)
+set(CMAKE_SYSTEM_PROCESSOR riscv)
 
-set(CMAKE_C_FLAGS "riscv32-unknown-elf")
-set(CMAKE_CXX_FLAGS "riscv32-unknown-elf")
-set(CMAKE_ASM_FLAGS "riscv32-unknown-elf")
+set(CLANG_TARGET_TRIPLE riscv32-none-elf)
+set(CMAKE_C_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
+set(CMAKE_CXX_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
+set(CMAKE_ASM_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
 
-set(CMAKE_LINKER "ld.lld")
-set(CMAKE_EXE_LINKER_FLAGS "--target=riscv32-unknown-elf -fuse-ld=lld -nostdlib")
 
-# which compilers to use for C and C++
-set(CMAKE_C_COMPILER   clang)
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-# where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc
-    /home/alex/mingw-install)
+set(CMAKE_FIND_ROOT_PATH root)
+
+# Don't run the linker on compiler check
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
