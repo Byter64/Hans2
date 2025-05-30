b118564f045897b1f989ae8b501507f999bd3c99
Byter64 <byter64@gmx.de>
2025-05-30 21:04:17 +0200

    Added second controller to controller.sv

--- HardwareDesign/Controller/controller.sv
+++ HardwareDesign/Controller/controller.sv
@@ -6,14 +6,20 @@ module Controller (
     input logic                              s_axil_rvalid,
     output logic                             s_axil_rready,
 
-    input  logic cont_data,
-    output logic cont_clk = 0,
-    output logic cont_activate
+    input  logic cont0_data,
+    output logic cont0_clk = 0,
+    output logic cont0_activate,
+
+    input  logic cont1_data,
+    output logic cont1_clk = 0,
+    output logic cont1_activate,
 );
 
-logic[11:0] controller_btns;
+logic[11:0] controller0_btns;
+logic[11:0] controller1_btns;
 
-logic[15:0] cont_state = 0;
+logic[15:0] cont0_state = 0;
+logic[15:0] cont1_state = 0;
 logic[10:0] state = START;
 localparam START = 0;
 localparam DATA_START = 3;
@@ -24,7 +30,7 @@ localparam DATA_END = 3 + 2 * 16; //16 states, each 2 fast clock cycles
 logic next_rvalid; //Assign your valid logic to this signal
 logic[31:0] next_rData; //Assign the data here
 assign next_rvalid = 1;
-assign next_rData = controller_btns;
+assign next_rData = {controller1_btns, controller0_btns};
 
 always_ff @(posedge aclk) begin
     if (!aresetn)
@@ -48,12 +54,12 @@ always_ff @(posedge clk) begin
     state <= state + 1;
 end
 
-assign cont_activate = state == 0 || state == 1;
+assign cont0_activate = state == 0 || state == 1;
 always_ff @(posedge clk) begin
     if(state >= DATA_START && state < DATA_END) begin
-        cont_clk <= ~cont_clk;
+        cont0_clk <= ~cont0_clk;
     end else begin
-        cont_clk <= 0;
+        cont0_clk <= 0;
     end
 end
 
@@ -70,13 +76,25 @@ end
 
 always_ff @(posedge inv_cont_clk) begin
     if(state >= DATA_START && state < DATA_END) begin
-        cont_state <= {~cont_data, cont_state[15:1]};
+        cont0_state <= {~cont0_data, cont0_state[15:1]};
     end
     if(state == DATA_END) begin
-        controller_btns <= cont_state[11:0];
+        controller0_btns <= cont0_state[11:0];
     end
     if(state > DATA_END) begin
-        cont_state <= 0;
+        cont0_state <= 0;
+    end
+end
+
+always_ff @(posedge inv_cont_clk) begin
+    if(state >= DATA_START && state < DATA_END) begin
+        cont1_state <= {~cont1_data, cont1_state[15:1]};
+    end
+    if(state == DATA_END) begin
+        controller1_btns <= cont1_state[11:0];
+    end
+    if(state > DATA_END) begin
+        cont1_state <= 0;
     end
 end
 
