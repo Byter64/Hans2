e2fdaed5db71d2446c0490235ff7d5d409b846b6
Byter64 <byter64@gmx.de>
2025-06-01 09:07:30 +0200

    One code say that the bootloader "works" now

--- Software/Bootloader/Bootloader.cpp
+++ Software/Bootloader/Bootloader.cpp
@@ -3,24 +3,19 @@
 	#include "DebugHelper.h"
 }
 
-#include <stdlib.h>
-#include <stdio.h>
 #include "ff.h"
 #include "Hapi.h"
-#include <errno.h>
-#include <string.h>
 
-FILE* elfFile;
+FIL elfFile;
 void* loadAddress = 0;
 
 static bool fileRead(el_ctx* ctx, void* dest, size_t nb, size_t offset)
 {
-	if (fseek(elfFile, offset, SEEK_SET))
+	if (f_lseek(&elfFile, offset))
 		return false;
 
-	int temp = fread(dest, nb, 1, elfFile);
-	ScreenPrint("fread");
-	if (temp != 1)
+	unsigned int bytesRead;
+	if (f_read(&elfFile, dest, nb, &bytesRead))
 		return false;
 
 	return true;
@@ -49,7 +44,7 @@ extern BYTE is_mounted;
 int main()
 {
 	char buffer[3];
-	//TODO: Add graphical progress bar
+
 	Hapi::Init();
 	FRESULT fatfsResult;
 	WaitFrame(120);
@@ -60,32 +55,30 @@ int main()
 	Hapi::Clear(Hapi::Color(0, 128, 128, 1));
 	Hapi::EndDrawing();
 #endif
-	const char* string1 = "Hallo Welt! Dies ist ein langer String";
-	ScreenPrint(string1);
-	for(int i = 0; i < 20; i++)
-	{
-		char* string2 = (char*)malloc(39);
-		strcpy(string2, string1);
-		ScreenPrint(string2);
-	}
 
-
-	//SetStatus("Mounting SD-Card...", 0, 20);
+	SetStatus("Mounting SD-Card...", 0, 20);
 	fatfsResult = f_mount(&FatFs, "", 0);
     is_mounted = 1;
 
-	//SetStatus(ByteToHex(fatfsResult, buffer), 10, 10);
+	if(fatfsResult)
+	{
+		ScreenPrint("Error:");
+		ScreenPrint(FRESULTToString(fatfsResult));
+		while(true);
+	}
+	SetStatus(ByteToHex(fatfsResult, buffer), 10, 10);
 
 	//Find main program to load
-	//SetStatus("Searching *.elf...", 15, 15);
+	SetStatus("Searching *.elf...", 15, 15);
 	DIR directory;
 	FILINFO fileInfo;
 	fatfsResult = f_findfirst(&directory, &fileInfo, "/", "*.elf");
 
-	//SetStatus(ByteToHex(fatfsResult, buffer), 20, 20);
-	if(fileInfo.fname[0] == '\0')
+	if(fileInfo.fname[0] == '\0' || fatfsResult)
 	{
-		//SetStatus("No .elf found", 20, 0);
+		ScreenPrint("Error:");
+		ScreenPrint(FRESULTToString(fatfsResult));
+		SetStatus("No .elf found", 20, 0);
 		while(true);
 	}
 	
@@ -105,49 +98,42 @@ int main()
 	debugMessage[i + 10] = '.';
 	debugMessage[i + 11] = '\0';
 	
-	//SetStatus(debugMessage, 25, 10);
-	elfFile = fopen(elfFilePath, "r");
-	setvbuf(elfFile, NULL, _IONBF, 0);
-	//SetStatus(elfFile ? "Open succeeded" : "Open failed", 30, 20);
-	if(!elfFile) while(true);
+	SetStatus(debugMessage, 25, 10);
+	fatfsResult = f_open(&elfFile, elfFilePath, FA_READ);
 
-	#ifdef DEBUG
-	loadAddress = malloc(32768);
-	if (!elfFile)
+	if(fatfsResult)
 	{
-		perror("File could not be opened");
-		return -1;
+		ScreenPrint("Error:");
+		ScreenPrint(FRESULTToString(fatfsResult));
+		SetStatus("Open failed", 20, 0);
+		while(true);
 	}
-	#endif
+	SetStatus("Open succeeded", 30, 20);
 	
 	el_ctx ctx;
 	ctx.pread = fileRead;
 	
-	//SetStatus("Initialising elfloader...", 35, 20);
+	SetStatus("Initialising elfloader...", 35, 20);
 	el_status result = el_init(&ctx);
 	check(result, "Init FAILED!");
 	
 	ctx.base_load_vaddr = (uintptr_t)loadAddress;
 	ctx.base_load_paddr = (uintptr_t)loadAddress;
 	
-	//SetStatus("Loading .elf file...", 40, 60);
+	SetStatus("Loading .elf file...", 40, 60);
 	result = el_load(&ctx, memoryAllocation);
 	check(result, "Load FAILED!");
 	
 	
-	//SetStatus("Resolving relocations...", 80, 10);
+	SetStatus("Resolving relocations...", 80, 10);
 	result = el_relocate(&ctx);
 	check(result, "Relocs FAILED!");
 	
 	uintptr_t entryPoint = ctx.ehdr.e_entry + (uintptr_t)loadAddress;
 	
-	#ifdef DEBUG
-	printf("Entry point within the program is address %p; Program is loaded at address %p\n", (uintptr_t)ctx.ehdr.e_entry, (uintptr_t)entryPoint);
-	printf("Jumping into loaded program. (This will not work on OSes)");
-	#endif // DEBUG
-	
-	//SetStatus("Have fun :)", 100, 30);
+	SetStatus("Succeeded. What a journey man, Have fun :)", 100, 45);
 	int (*loadedMain)() = (int (*)())entryPoint;
+	while(true);
 	loadedMain();
 	
 	while (true);
