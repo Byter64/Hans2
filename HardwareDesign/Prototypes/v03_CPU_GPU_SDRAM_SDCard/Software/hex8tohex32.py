74f72aa9b13e2a602543b8f564d31e907a8475ce
Byter <yannik.stamm@gmx.net>
2025-06-05 12:53:34 +0200

    Reverted hex8tohex32.py Moved loading address back to BOOTLOADER_START in riscv.ld

--- HardwareDesign/Prototypes/v03_CPU_GPU_SDRAM_SDCard/Software/hex8tohex32.py
+++ HardwareDesign/Prototypes/v03_CPU_GPU_SDRAM_SDCard/Software/hex8tohex32.py
@@ -9,20 +9,20 @@ data = []
 def write_data():
     if len(data) != 0:
         print("@%08x" % (ptr >> 2))
-        while len(data) % 2 != 0:
+        while len(data) % 4 != 0:
             data.append(0)
-        for word_bytes in zip(*([iter(data)]*2)):
+        for word_bytes in zip(*([iter(data)]*4)):
             print("".join(["%02x" % b for b in reversed(word_bytes)]))
 
 for line in fileinput.input():
     if line.startswith("@"):
         addr = int(line[1:], 16)
         addr -= 0x2010000
-        if addr > ptr+2:
+        if addr > ptr+4:
             write_data()
             ptr = addr
             data = []
-            while ptr % 2 != 0:
+            while ptr % 4 != 0:
                 data.append(0)
                 ptr -= 1
         else:
