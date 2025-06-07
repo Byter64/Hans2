3e65b751f10a9f7c6ebe5a45ed42d9c6aaac7245
Byter64 <byter64@gmx.de>
2025-06-07 19:03:55 +0200

    Recompiled assembly

--- HardwareDesign/Prototypes/v05_with_Vex/Software/main.cpp
+++ HardwareDesign/Prototypes/v05_with_Vex/Software/main.cpp
@@ -1,23 +1,8 @@
-﻿
+﻿volatile int* p = (int*) 0x2016000;
 int main() {
-    volatile int x = 5000;
-    volatile int y = 1000;
-    volatile int z = 0;
-    for(int i = 0; i < 1000; i++) {
-        z += x + y;
+    for(int i = 0; i < 256; i++)
+    {
+        *(p + i) = i;
     }
-    for(int i = 0; i < 1000; i++) {
-        x += z + y;
-    }
-    for(int i = 0; i < 1000; i++) {
-        y += x + z;
-    }
-    volatile int *p = (int*)10;
-    *p = x;
-    *p = y;
-    *p = z;
-
-    while (true);
-    
     return 0;
 }
